3e7eb367f881c75db1b4a45260ec9b62416c189d
ferdiaz93 <fernandodiaz12@gmail.com>
2019-09-04 06:19:04 -0300

    aplicacion terminada

--- public/scripts/ajax.js
+++ public/scripts/ajax.js
@@ -1,5 +1,5 @@
 const urlMenu = "http://remote.fizzmod.com/menu.json"
-const urlBanner = "http://remote.fizzmod.com/body.json"
+const urlBody = "http://remote.fizzmod.com/body.json"
 
 
 /**
@@ -10,28 +10,21 @@ function pedirMenu(callback){
     fetch(urlMenu)
     .then(response => response.json())
     .then(response => callback(response.menu.categories))
+    .catch((error)=>{
+        console.log(error);
+    })
 }
-
-
 
 /**
  * 
  * @param {funcion} callback 
  * pide las rutas de las imagenes y el href
  */
-function pedirImagenes(callback){
-    fetch(urlBanner)
+function pedirBody(callback){
+    fetch(urlBody)
     .then(response => response.json())
-    .then(response => callback(response.slides))
-}
-
-/**
- * 
- * @param {funcion} callback
- * funcion que pide todos los productos de la api
- */
-function pedirProductos(callback){
-    fetch(urlBanner)
-    .then(response => response.json())
-    .then(response => callback(response.products))
+    .then(response => callback(response))
+    .catch((error)=>{
+        console.log(error);
+    })
 }
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,11 +2,26 @@ const divMenu = document.getElementById("menu");
 const divsImg = document.getElementsByClassName("carousel-item");
 const contCarousel = document.getElementById("contenedor-carrousel");
 const divContenedor = document.getElementById("contenedor");
+const divFiltros = document.getElementById("filtros");
 const direccionActual = location.href;
 
+let pedidoBody;
+
 pedirMenu(crearMenu)
-pedirImagenes(crearImagenesCarousel);
-pedirProductos(crearTodosProductos)
+pedirBody(hacerPedidos);
+
+/**
+ * 
+ * @param {objeto} datos es un objeto con todo el contenido del body
+ * 
+ * esta funcion ejecuta las funciones necesarias al abrir la aplicacion 
+ */
+function hacerPedidos(datos) {
+    pedidoBody = datos;
+    crearImagenesCarousel(datos.slides);
+    crearTodosProductos(datos.products);
+    recorrerFiltros(datos.filters);
+}
 
 /**
  * 
@@ -17,14 +32,18 @@ function crearMenu(datos) {
     //se crea un Fragment para guardar dentro todos los elementos <a> que se vayan a crear
     let frag = document.createDocumentFragment();
 
-    datos.map(e => {
+    datos.map(elemento => {
         let nuevoDiv = document.createElement("div");
         let nuevoLink = document.createElement("a");
-        nuevoLink.setAttribute("href", e.href);
+        nuevoLink.setAttribute("href", elemento.href);
         nuevoLink.setAttribute("class", "linksMenu");
 
-        nuevoLink.innerText = e.title;
+        nuevoLink.innerText = elemento.title;
         nuevoDiv.appendChild(nuevoLink)
+
+        nuevoDiv.addEventListener("click", ()=>{
+            location.href = direccionActual + elemento.href
+        })
         frag.appendChild(nuevoDiv)
     });
     //se agrega el fragment al div del DOM
@@ -38,7 +57,7 @@ function crearMenu(datos) {
  */
 function crearImagenesCarousel(datos) {
 
-    for (let i = 0; i < datos.length; i++) {
+    datos.map((e, i) => {
         let divEnlace = document.createElement("div");
 
         if (i === 0) {
@@ -47,30 +66,32 @@ function crearImagenesCarousel(datos) {
             divEnlace.setAttribute("class", "carousel-item")
         }
         let aImg = document.createElement("a");
-        aImg.setAttribute("href", datos[i].href);
+        aImg.setAttribute("href", e.href);
         let nodoImg = document.createElement("img");
         nodoImg.setAttribute("class", "d-block w-100");
-        nodoImg.setAttribute("alt", datos[i].imgName);
-        nodoImg.setAttribute("src", `./assets/images/${datos[i].imgName}`);
+        nodoImg.setAttribute("alt", e.imgName);
+        nodoImg.setAttribute("src", `./assets/images/${e.imgName}`);
 
         aImg.appendChild(nodoImg);
         divEnlace.appendChild(aImg);
         contCarousel.appendChild(divEnlace);
-    }
+    })
 }
+
 /**
  * 
- * @param {objeto} datos recibe un objeto con los datos del producto 
+ * @param {objeto} producto recibe un objeto con los datos del producto 
  * 
  * funcion que crea el producto y su info en el DOM
  */
 function crearProducto(producto) {
+    //En caso de que haya descuento se coloca el precio anterior en el h5
     let h5;
 
     let divCont = document.createElement("div");
     divCont.setAttribute("class", "contenedor-producto");
 
-    // creacion del div que contiene el span y la imagen del producto
+    // Creacion del div que contiene el span y la imagen del producto
     let divContInfo = document.createElement("div");
     divContInfo.setAttribute("class", "imgProduct");
     //TITULO DEL PRODUCTO
@@ -79,41 +100,54 @@ function crearProducto(producto) {
     //IMAGEN DEL PRODUCTO
     let img = document.createElement("img");
     img.setAttribute("src", `./assets/images/${producto.image}`);
-
+    //PRECIO DEL PRODUCTO
     let h4 = document.createElement("h4");
     h4.innerText = `$ ${producto.price.sellingPrice}`;
 
+    // EN CASO DE NO TENER DESCUENTO NO SE CREA EL SPAN
     if (producto.price.listPrice !== producto.price.sellingPrice) {
+        //se guarda el porcentaje en una variable
         let porcentajeDif = sacarPorcentaje(producto.price.sellingPrice, producto.price.listPrice)
+        //Se crea el span y se le agrega el porcentaje dentro
         let span = document.createElement("span");
         span.innerHTML = `${porcentajeDif}%`;
-
+        // se crea el h5 y se le agrega el precio viejo (tachado)
         h5 = document.createElement("h5");
         h5.innerText = `$ ${producto.price.listPrice}`;
 
         divContInfo.appendChild(span);
 
     } else {
+        //Si no hay diferencia de precios el h5 estará vacio
         h5 = document.createElement("h5");
         h5.innerText = " ";
 
     }
 
+    //se appendea todo al div que contiene a cada este producto
     divContInfo.appendChild(img);
     divContInfo.appendChild(h3);
     divContInfo.appendChild(h5);
     divContInfo.appendChild(h4);
-
+    //se appendea todo al div que solo va a mantener este producto
     divCont.appendChild(divContInfo)
 
-    divCont.addEventListener("click", ()=>{
+    //se le agrega un evento al div para redireccionar segun el producto que clickee
+    divCont.addEventListener("click", () => {
         location.href = direccionActual + producto.href
     })
+    //se appendea al div del DOM que contiene a todos los productos
     divContenedor.appendChild(divCont);
 }
 
 
-
+/**
+ * 
+ * @param {Number} precioNuevo 
+ * @param {Number} precioViejo 
+ * @return retorna el resultado de la operacion
+ * Esta funcion recibe dos numeros y devuelve el porcentaje de la diferencia que tienen esos numeros
+ */
 function sacarPorcentaje(precioNuevo, precioViejo) {
     let division = precioNuevo / precioViejo;
     let porcentaje = division * 100;
@@ -122,10 +156,121 @@ function sacarPorcentaje(precioNuevo, precioViejo) {
     return resultado;
 }
 
-
-function crearTodosProductos(datos){
-    for(let i = 0; i < datos.length; i++){
+/**
+ * 
+ * @param {array} datos contiene todos los productos en un array
+ * 
+ * Esta funcion recorre todos los productos y ejecuta la funcion crearProducto por cada elemento 
+ */
+function crearTodosProductos(datos) {
+    for (let i = 0; i < datos.length; i++) {
         crearProducto(datos[i]);
     }
 }
 
+
+/**
+ * 
+ * @param {objeto} datos recibe un objeto con las propiedades de los filtros
+ * @return retorna un div que contienen los filtros 
+ * Esta funcion crea nodos con las propiedades del objeto que recibe
+ */
+
+function crearFiltros(datos) {
+
+    let divContenedor = document.createElement("div");
+
+    let h3 = document.createElement("h3");
+    h3.innerText = datos.title;
+    divContenedor.appendChild(h3);
+    let pContenedor = document.createElement("p");
+    pContenedor.setAttribute("id", datos.field);
+    pContenedor.setAttribute("class", "secciones");
+
+    datos.values.map((e,i)=>{
+
+        let input = document.createElement("input");
+        input.setAttribute("class", "inputCheck");
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("id", e);
+        
+        let label = document.createElement("label");
+        label.setAttribute("for", e);
+        label.innerText = e;
+        
+        pContenedor.appendChild(input);
+        pContenedor.appendChild(label);
+        
+    }) 
+    
+    divContenedor.appendChild(pContenedor);
+
+    return divContenedor;
+
+
+}
+/**
+ * 
+ * @param {array} datos recibe un array de objetos
+ * 
+ * funcion que reccore el array de objetos y crea los filtros 
+ */
+function recorrerFiltros(datos) {
+    let fragmentContenedor = document.createDocumentFragment();
+
+    for (let i = 0; i < datos.length; i++) {
+        let divContenedor = crearFiltros(datos[i]);
+        fragmentContenedor.appendChild(divContenedor);
+
+    }
+    divFiltros.appendChild(fragmentContenedor);
+}
+
+/**
+ * Se le agrega evento al div de los filtros para poder sacar las propiedades
+ * necesarias para aplicar los filtros
+ */
+divFiltros.addEventListener("click", (e) => {
+
+    const todosLosChecks = document.getElementsByClassName("inputCheck");
+    let filtros = [];
+
+    for (let i = 0; i < todosLosChecks.length; i++) {
+        if (todosLosChecks[i].checked === true) {
+            filtros.push(todosLosChecks[i].id);
+        }
+    }
+    if (filtros.length == 0) {
+        divContenedor.innerHTML = " ";
+        crearTodosProductos(pedidoBody.products);
+        return;
+    }
+    //console.log(filtros);
+    aplicarFiltros(filtros, pedidoBody.products);
+
+})
+
+/**
+ * 
+ * @param {array} filtros array de strings con filtros a aplicar 
+ * @param {array} arrayDeObj array de objetos a quienes aplicar los filtros
+ * 
+ * esta funcion boora el contenido del div contenedor 
+ * aplica los filtros y ejecuta otra funcion con un array de objetos
+ */
+function aplicarFiltros(filtros, arrayDeObj) {
+    let arrayAplicados = []
+    divContenedor.innerHTML = " ";
+    for (let i = 0; i < filtros.length; i++) {
+        for (let x = 0; x < arrayDeObj.length; x++) {
+            for (let y = 0; y < arrayDeObj[x].attributes.length; y++) {
+                if (filtros[i] == arrayDeObj[x].attributes[y].value) {
+                    arrayAplicados.push(arrayDeObj[x]);
+                }
+            }
+        }
+    }
+
+    crearTodosProductos(arrayAplicados);
+
+}
